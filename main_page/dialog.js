686ba775debeca3f03519738df4914b8dd7fd4f4
Eugene <jhongil3007@gmail.com>
2024-03-05 01:15:25 +0300

    Все описания тегов хранятся в json

--- main_page/dialog.js
+++ main_page/dialog.js
@@ -1,15 +1,14 @@
+import data from './data.json' assert {type: "json"};
+
+const message_sections = data;
 const templateNode = document.querySelector("#messageTemplate");
 const dialogTemplate = templateNode.content.cloneNode(true);
 const dialog = dialogTemplate.querySelector("#dialogWindow");
 const message = dialogTemplate.querySelector("p");
 
-const fs = require('fs');
-const readFile = fs.readFileSync('data_messages.json', 'utf8');
-console.log(readFile);
-
-function open_dialog(key, id) {
+window.open_dialog = function (key, id) {
     const divBlock = document.querySelector(id);
     divBlock.append(dialogTemplate);
     message.innerHTML = message_sections[key];
-    dialog.show();
+    dialog.showModal();
 }
