4e41ba290ed40a471d3e1f3713b93fd9e9000332
Eugene <jhongil3007@gmail.com>
2024-03-06 20:23:25 +0300

    Исправление модального окна

--- main_page/dialog.js
+++ main_page/dialog.js
@@ -1,14 +1,14 @@
-import data from './data.json' with {type: "json"};
+fetch('./main_page/data.json').then(response => response.json()).then(data => {
+    const message_sections = data;
+    const templateNode = document.getElementById("messageTemplate");
+    const dialogTemplate = templateNode.content.cloneNode(true);
+    const dialog = dialogTemplate.getElementById("dialogWindow");
+    const message = dialogTemplate.getElementById("message");
 
-const message_sections = data;
-const templateNode = document.getElementById("messageTemplate");
-const dialogTemplate = templateNode.content.cloneNode(true);
-const dialog = dialogTemplate.getElementById("dialogWindow");
-const message = dialogTemplate.getElementById("message");
-
-window.open_dialog = function (key, id) {
-    const divBlock = document.querySelector(id);
-    divBlock.append(dialogTemplate);
-    message.innerHTML = message_sections[key];
-    dialog.show();
-}
+    window.open_dialog = function (key, id) {
+        const divBlock = document.querySelector(id);
+        divBlock.append(dialogTemplate);
+        message.innerHTML = message_sections[key];
+        dialog.showModal();
+    }
+})
