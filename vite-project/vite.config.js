70a575d5365166b1de022f38dd6b749f55de44f4
eugenepurp <jhongil3007@gmail.com>
2024-03-21 19:16:35 +0300

    изменение config

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,8 +1,20 @@
 import Inspect from 'vite-plugin-inspect';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default {
     base: "/html-tags.github.io",
     plugins: [
         Inspect(),
+        createHtmlPlugin({
+            title: 'HTML теги',
+            filename: 'index.html',
+            template: 'index.html'
+        },
+        {
+            title: 'W3C',
+            filename: 'w3c_page.html',
+            template: 'w3c/w3c_page.html'
+        }
+        )
     ],
 }; 
