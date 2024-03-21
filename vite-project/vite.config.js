20c9d7d3e0ee58fee3cb507492eca937a5b07348
eugenepurp <jhongil3007@gmail.com>
2024-03-21 19:24:17 +0300

    config

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -6,15 +6,19 @@ export default {
     plugins: [
         Inspect(),
         createHtmlPlugin({
-            title: 'HTML теги',
-            filename: 'index.html',
-            template: 'index.html'
-        },
-        {
-            title: 'W3C',
-            filename: 'w3c_page.html',
-            template: 'w3c/w3c_page.html'
-        }
-        )
+            filename: "index.html",
+            template: "index.html",
+            minify: true,
+        }),
+        createHtmlPlugin({
+            filename: "file_loader.html",
+            template: "file_loader/file_loader.html",
+            minify: true,
+        }),
+        createHtmlPlugin({
+            filename: "w3c_page.html",
+            template: "w3c/w3c_page.html",
+            minify: true,
+        })
     ],
 }; 
