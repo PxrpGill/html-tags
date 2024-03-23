efe75aa6bedeac9392ae8bcb87efb1a57a628d5a
eugenepurp <jhongil3007@gmail.com>
2024-03-23 14:37:30 +0300

    config

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -2,7 +2,7 @@ import Inspect from 'vite-plugin-inspect';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default {
-    base: "/html-tags.github.io",
+    base: "/html-tags.github.io/",
     plugins: [
         Inspect(),
         createHtmlPlugin({
