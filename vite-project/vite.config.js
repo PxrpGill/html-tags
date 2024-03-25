f352ea2f66258f71c2fad272bb1c934686db31ad
eugenepurp <jhongil3007@gmail.com>
2024-03-25 20:19:58 +0300

    config

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -5,5 +5,20 @@ export default {
     base: "/html-tags.github.io/",
     plugins: [
         Inspect(),
+        createHtmlPlugin({
+            title: "Index Page",
+            template: "../index.html",
+            minify: true,
+        }),
+        createHtmlPlugin({
+            title: "W3C",
+            template: "../w3c/w3c_page.html",
+            minify: true,
+        }),
+        createHtmlPlugin({
+            title: "File Loader",
+            template: "../file_loader/file_loader.html",
+            minify: true
+        })
     ],
 }; 
